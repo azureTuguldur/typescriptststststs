bdba4255548f7bb12beed3dd79784105ee244127
Evening <evening@admins-iMac-2.local>
2023-04-03 17:34:37 +0800

    sre

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const ner = "Boldoo";
+const age = 21;
+console.log("Name: ", ner);
+console.log("Age: ", age);
+function add(a, b) {
+    console.log("Sum: ", a + b);
+}
+add(2, 52);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const ner: string = "Boldoo";
+const age: number = 21;
+console.log("Name: ", ner);
+console.log("Age: ", age);
+
+function add(a: number, b: number): void {
+  console.log("Sum: ", a + b);
+}
+add(2, 52);
